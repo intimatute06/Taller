ee7afaa766c4a6bcaf873ceb410f84cfd65704fe
intimatute06 <168133152+intimatute06@users.noreply.github.com>
2024-05-10 15:35:22 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -5,15 +5,26 @@ int main(void) {
     float arriendo = 400;
     float sueldo = 4000;
     float totalgast;
-    
     float primerahorro = 0;
 
     for (int mes = 1; mes <= 6; mes++) {
         printf("\nMes %d\n", mes);
         totalgast = calcularTotalGastos(arriendo, sueldo);
         printf("Su total de gastos es: %.2f\n", totalgast);
-
         
+        float ingresosmes, ventas, inversiones, totaling, ingtotal;
+        printf("Ingrese los ingresos del mes:");
+        scanf("%f", &ingresosmes);
+        printf("Ingrese las ventas:");
+        scanf("%f", &ventas);
+        printf("Ingrese las inversiones:");
+        scanf("%f", &inversiones);
+        
+        totaling = ingresosmes + ventas + inversiones;
+        printf("Su ingreso es: %.2f\n", totaling);
+        
+        ingtotal = totaling - totalgast;
+        printf("Su total de ingresos es: %.2f\n", ingtotal);
     }
 
     return 0;
